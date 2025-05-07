c9210275b67a3c3aef1876d22916bf764f50c11b
Michel Azzam <azzam3michel@gmail.com>
2025-05-07 20:15:06 +0300

    all origin

--- src/server.ts
+++ src/server.ts
@@ -24,12 +24,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // allow cores all origins
-app.use(
-  cors({
-    origin: ["http://localhost:8080", "https://staging.liquorstorechat.com"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // global middleware
 app.use(attachUserToReq);
